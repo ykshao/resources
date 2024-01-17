9e0d6d9a71d62b7f18a267bd3a72878040c737bb
ykshao <lovemekaikai@163.com>
2024-01-17 11:55:31 +0800

    Update drag.js

--- drag.js
+++ drag.js
@@ -1,1 +1,9 @@
-console.log('hello 124');
+var element = document.querySelector('h2');
+var button = document.createElement('button');
+button.innerHTML = 'Click me';
+
+element.insertAdjacentElement('beforebegin', button);
+
+button.addEventListener('click', function() {
+  alert('Button clicked!');
+});
